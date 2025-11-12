b9c5da79af008f90de76338f31b7b0ac76e949e6
ivanovichi <ivan.arthur18@gmail.com>
2025-11-12 13:27:16 -0600

    Add jest configuration
    I have added the dependency for jest as well as
    set up jest to work with webpack, modules and
    eslint

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
 import "./style.css";
+const hello = "hello";
 for (let i = 1; i < 5; i++) {
   console.log(i);
 }
+
+console.log(hello);
+
+//Function for the test
+function sum(a, b) {
+  return a + b;
+}
+
+export { sum };
